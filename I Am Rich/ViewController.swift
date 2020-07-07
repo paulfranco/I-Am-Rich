18325a06bbc7ba3a1977d2dcaca15abc245eea99
Paul Franco <mail@paulfran.co>
2020-07-07 00:47:57 -0700

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Paul Franco on 7/7/20.
+//  Copyright © 2020 Paul Franco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
